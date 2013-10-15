819c2d0ed25dceb53ce94a96e5ba4ef76e05e171
user <chengjoanne2@gmail.com>
2013-10-15 09:31:58 -0400

    Automated commit at 2013-10-15 13:31:58 UTC by middleman-deploy 0.1.3

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,83 +1,119 @@
 (function() {
-  $(function() {
-    var formatDate, language, languages, loadPushCountsSparkline, loadWatchCountsSparkline, _i, _len, _results;
-    formatDate = function(date) {
-      return date.toLocaleDateString();
-    };
-    loadWatchCountsSparkline = function(language) {
-      var container;
-      container = $('#watch_counts');
+  var PushCounts, WatchCounts,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  WatchCounts = (function() {
+    function WatchCounts() {
+      this.populateTable = __bind(this.populateTable, this);
+      this.loadData = __bind(this.loadData, this);
+      this.el = $('#watch_counts');
+    }
+
+    WatchCounts.prototype.loadData = function(language) {
+      var _this = this;
       return $.ajax({
         type: 'GET',
         url: "watch_data/" + language + "_watches_github.csv",
         data: null,
         success: function(data) {
-          var dates, line, lines, max, maxDate, min, minDate, points, _i, _len;
-          lines = data.split(/\n/).slice(1);
-          points = [];
-          dates = [];
-          for (_i = 0, _len = lines.length; _i < _len; _i++) {
-            line = lines[_i];
-            dates.push(line.split(',')[0]);
-            points.push(parseInt(line.split(',')[2]));
-          }
-          min = _.min(points);
-          max = _.max(points);
-          minDate = dates[_.indexOf(points, min)];
-          maxDate = dates[_.indexOf(points, max)];
-          container.find("#" + language + "-sparkline").sparkline(points.slice(0, points.length - 1), {
-            width: 300,
-            height: 40,
-            fillColor: '#fefefe',
-            lineColor: '#101010'
-          });
-          container.find("#" + language + " .min").html(min);
-          container.find("#" + language + " .min-date").html(minDate);
-          container.find("#" + language + " .max").html(max);
-          return container.find("#" + language + " .max-date").html(maxDate);
+          return _this.populateTable(data, language);
         }
       });
     };
-    loadPushCountsSparkline = function(language) {
-      var container;
-      container = $('#push_counts');
+
+    WatchCounts.prototype.populateTable = function(data, language) {
+      var dates, line, lines, max, maxDate, min, minDate, points, _i, _len;
+      lines = data.split(/\n/).slice(1);
+      points = [];
+      dates = [];
+      for (_i = 0, _len = lines.length; _i < _len; _i++) {
+        line = lines[_i];
+        dates.push(line.split(',')[0]);
+        points.push(parseInt(line.split(',')[2]));
+      }
+      min = _.min(points);
+      max = _.max(points);
+      minDate = dates[_.indexOf(points, min)];
+      maxDate = dates[_.indexOf(points, max)];
+      this.el.find("#" + language + "-sparkline").sparkline(points.slice(0, points.length - 1), {
+        width: 300,
+        height: 40,
+        fillColor: '#fefefe',
+        lineColor: '#101010'
+      });
+      this.el.find("#" + language + " .min").html(min);
+      this.el.find("#" + language + " .min-date").html(minDate);
+      this.el.find("#" + language + " .max").html(max);
+      return this.el.find("#" + language + " .max-date").html(maxDate);
+    };
+
+    return WatchCounts;
+
+  })();
+
+  PushCounts = (function() {
+    function PushCounts() {
+      this.populateTable = __bind(this.populateTable, this);
+      this.loadData = __bind(this.loadData, this);
+      this.el = $('#push_counts');
+    }
+
+    PushCounts.prototype.loadData = function(language) {
+      var _this = this;
       return $.ajax({
         type: 'GET',
         url: "push_data/" + language + "_github2.csv",
         data: null,
         success: function(data) {
-          var dates, line, lines, max, maxDate, min, minDate, points, _i, _len;
-          lines = data.split(/\n/).slice(1);
-          points = [];
-          dates = [];
-          for (_i = 0, _len = lines.length; _i < _len; _i++) {
-            line = lines[_i];
-            dates.push(Date.parse(line.split(',')[0]));
-            points.push(parseInt(line.split(',')[2]));
-          }
-          min = _.min(points);
-          max = _.max(points);
-          minDate = new Date(dates[_.indexOf(points, min)]);
-          maxDate = new Date(dates[_.indexOf(points, max)]);
-          container.find("#" + language + "-sparkline").sparkline(points.slice(0, points.length - 1), {
-            width: 300,
-            height: 40,
-            fillColor: '#fefefe',
-            lineColor: '#101010'
-          });
-          container.find("#" + language + " .min").html(min);
-          container.find("#" + language + " .min-date").html(formatDate(minDate));
-          container.find("#" + language + " .max").html(max);
-          return container.find("#" + language + " .max-date").html(formatDate(maxDate));
+          return _this.populateTable(data, language);
         }
       });
     };
+
+    PushCounts.prototype.populateTable = function(data, language) {
+      var dates, line, lines, max, maxDate, min, minDate, points, _i, _len;
+      lines = data.split(/\n/).slice(1);
+      points = [];
+      dates = [];
+      for (_i = 0, _len = lines.length; _i < _len; _i++) {
+        line = lines[_i];
+        dates.push(Date.parse(line.split(',')[0]));
+        points.push(parseInt(line.split(',')[2]));
+      }
+      min = _.min(points);
+      max = _.max(points);
+      minDate = new Date(dates[_.indexOf(points, min)]);
+      maxDate = new Date(dates[_.indexOf(points, max)]);
+      this.el.find("#" + language + "-sparkline").sparkline(points.slice(0, points.length - 1), {
+        width: 300,
+        height: 40,
+        fillColor: '#fefefe',
+        lineColor: '#101010'
+      });
+      this.el.find("#" + language + " .min").html(min);
+      this.el.find("#" + language + " .min-date").html(this.formatDate(minDate));
+      this.el.find("#" + language + " .max").html(max);
+      return this.el.find("#" + language + " .max-date").html(this.formatDate(maxDate));
+    };
+
+    PushCounts.prototype.formatDate = function(date) {
+      return date.toLocaleDateString();
+    };
+
+    return PushCounts;
+
+  })();
+
+  $(function() {
+    var language, languages, pushCounts, watchCounts, _i, _len, _results;
+    watchCounts = new WatchCounts();
+    pushCounts = new PushCounts();
     languages = ['coffeescript', 'javascript', 'ruby', 'python', 'go', 'haskell', 'clojure', 'scala', 'viml', 'php', 'elixir', 'erlang', 'java'];
     _results = [];
     for (_i = 0, _len = languages.length; _i < _len; _i++) {
       language = languages[_i];
-      loadPushCountsSparkline(language);
-      _results.push(loadWatchCountsSparkline(language));
+      pushCounts.loadData(language);
+      _results.push(watchCounts.loadData(language));
     }
     return _results;
   });
